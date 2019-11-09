59394c83484ff03cc986b9d2e867be75ce55cd2d
dotchetter <dotchetter@protonmail.ch>
2019-11-09 19:38:35 +0100

    Added base class Customer, bare minimums.

--- AdvertIce.cpp
+++ AdvertIce.cpp
@@ -1,1 +1,44 @@
-// Solution 
+#include <iostream>
+#include <vector>
+#include <cstdlib>
+
+/* 
+Advertice source code.
+Feature: Customer class
+
+Specified requirements according to design doc:
+
+Name: String
+ID: Int
+Campaigns: Vector
+
+*/
+
+using namespace std;
+
+class Customer {
+	string name;
+	int id;
+	vector<string> campaigns;
+
+public:
+	Customer(string name, int id)
+	{
+		this->name = name;
+		this->id = id;
+	}
+
+	string GetName() {
+		return name;
+	}
+
+	int GetId() {
+		return id;
+	}
+};
+
+int main() {
+	Customer t("test", 123);
+	cout << t.GetName();
+
+}
