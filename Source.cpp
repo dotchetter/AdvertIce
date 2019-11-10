5a00f1d6f55395dac7f2ca0396031e79ec3dea98
dotchetter <dotchetter@protonmail.ch>
2019-11-10 23:05:25 +0100

    linting, approval of commited code.

--- Source.cpp
+++ Source.cpp
@@ -10,62 +10,66 @@ using namespace std;
 
 class Ad 
 {
+
 private:
+
 	string name;
-	string adText;
-	string adType;
+	string adText; 
+	string adType; // Mockup
 	int id;
-	int GenerateUniqueId()
-	{
-		
-		id = rand();
-		return id;
-		
-	}
-			
+
 public:
-	Ad(string name,string text)
+
+	Ad(string name, string text)
 	{
 		this->name = name;
-		this->adText = text;
-		
+		this->adText = text;	
 	}
+
 	string GetName()
 	{
 		return name;
 	}
-	void SetName()
-	{}
+
+	void SetName(string name)
+	{
+		this->name = name; // Inkludera felhantering och datavalidering
+	}
+
 	int GetId()
 	{
-		
-		{
-			return id;
-		}
-		
+		return id;
 	}
-	void SetId()
+
+	void SetId(int id)
 	{
-		this->id = GenerateUniqueId();
+		this->id = id;
 	}
+
 	string GetType()
 	{
 		return adType;
 	}
-	void SetType()
-	{}
+
+	void SetType(AdType type)
+	{
+		this->adtype = type
+	}
+
 	string GetText()
 	{
 		return adText;
 	}
-	void SetText()
-	{}
+
+	void SetText(string adText)
+	{
+		this->adText = adText;
+	}
 	
 };
 
 int main()
 {
-	srand((unsigned long int)time(0));
 
 	Ad ad("Anton Norell\n", "Norell och Nilsson bilskador Abn");
 	cout << ad.GetName() << ad.GetText() << endl;
