fcb915a77b0e4195f765c5c959915e3f7e3ce91f
dotchetter <dotchetter@protonmail.ch>
2019-11-11 21:00:26 +0100

    Included adtype.h for fully integrated classes, removed mockup variables. Deleted Ad.cpp, logic moved to Ad.h

--- Ad.h
+++ Ad.h
@@ -1,1 +1,68 @@
 #pragma once
+#include <string>
+#include <vector>
+#include <iostream>
+#include <exception>
+#include "AdType.h"
+
+using namespace std;
+
+class Ad
+{
+
+private:
+
+	string name;
+	string adText;
+	AdType adType; 
+	int id;
+
+public:
+
+	Ad(string name, string text)
+	{
+		this->name = name;
+		this->adText = text;
+	}
+
+	string GetName()
+	{
+		return name;
+	}
+
+	void SetName(string name)
+	{
+		this->name = name; // Inkludera felhantering och datavalidering
+	}
+
+	int GetId()
+	{
+		return id;
+	}
+
+	void SetId(int id)
+	{
+		this->id = id;
+	}
+
+	AdType GetType()
+	{
+		return adType;
+	}
+
+	void SetType(AdType type)
+	{
+		this->adType = type
+	}
+
+	string GetText()
+	{
+		return adText;
+	}
+
+	void SetText(string adText)
+	{
+		this->adText = adText;
+	}
+
+};
